3be2aa6ef4da169f7e73a6aaeec630a8b1beecfa
Varma Cephas <varmac231@gmail.com>
2024-08-15 16:42:56 +0300

    add currency conversion calculation and output

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const converted_currency_value_output = document.querySelector(".converted_curre
 const origin_currency_conversion_rate = document.querySelector(".origin_currency_conversion_rate");
 const converted_currency_conversion_rate = document.querySelector(".converted_currency_conversion_rate");
 
+let final_converted_currency;
+
 const _0x1dd5a7=_0x22d0;function _0x22d0(_0x1a75ea,_0x475155){const _0x24a4ea=_0x24a4();return _0x22d0=function(_0x22d0fa,_0x3501f8){_0x22d0fa=_0x22d0fa-0xb8;let _0x21f3d3=_0x24a4ea[_0x22d0fa];return _0x21f3d3;},_0x22d0(_0x1a75ea,_0x475155);}(function(_0x5a884b,_0x34ef21){const _0x3b5d3b=_0x22d0,_0x2f1ea0=_0x5a884b();while(!![]){try{const _0x3f256a=-parseInt(_0x3b5d3b(0xbd))/0x1+-parseInt(_0x3b5d3b(0xb8))/0x2*(-parseInt(_0x3b5d3b(0xc0))/0x3)+parseInt(_0x3b5d3b(0xbe))/0x4+-parseInt(_0x3b5d3b(0xb9))/0x5+parseInt(_0x3b5d3b(0xbb))/0x6+-parseInt(_0x3b5d3b(0xbc))/0x7+parseInt(_0x3b5d3b(0xbf))/0x8;if(_0x3f256a===_0x34ef21)break;else _0x2f1ea0['push'](_0x2f1ea0['shift']());}catch(_0x4d4859){_0x2f1ea0['push'](_0x2f1ea0['shift']());}}}(_0x24a4,0x1c064));function _0x24a4(){const _0x71c0be=['812xDxnhf','1696688OBAJkp','15xowcHT','71986uZwUUb','588940gZhAfQ','jcKGTcMWAkvwFccF5Czf7pZkJzR0DKAwRphhm6NE','112764oVsWtc','885948ZdUEOP','51908ziilqS'];_0x24a4=function(){return _0x71c0be;};return _0x24a4();}const api_key=_0x1dd5a7(0xba);
 
 async function getCurrencyData(){
@@ -44,5 +46,7 @@ convert_currency_select_option.addEventListener("change", ()=>{
 })
 
 currency_converter_form.addEventListener("submit", (event)=>{
+    final_converted_currency = `${((Number(currency_amount_input.value) / Number(origin_currency_select_option.value.split(" ")[0])) * Number(convert_currency_select_option.value.split(" ")[0])).toFixed(4)} ${convert_currency_select_option.value.split(" ")[1]}`;
+    converted_currency_value_output.textContent = final_converted_currency;
     event.preventDefault();
 })
